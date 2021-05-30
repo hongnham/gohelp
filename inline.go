5f4942d1fef471f2bcefb2b8843c807cb4a43091
pham hong nham <70361369+hongnham@users.noreply.github.com>
2021-05-30 19:19:49 +0700

    Create inline.go

---
+++ inline.go
@@ -0,0 +1,31 @@
+package gcc
+
+type If bool
+
+func (c If) Int64(a, b int64) int64 {
+	if c {
+		return a
+	}
+	return b
+}
+
+func (c If) String(a, b string) string {
+	if c {
+		return a
+	}
+	return b
+}
+
+func (c If) Int(a, b int) int {
+	if c {
+		return a
+	}
+	return b
+}
+func (c If) Float64(a, b float64) float64 {
+	if c {
+		return a
+	}
+	return b
+}
+
