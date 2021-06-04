22017cfa06562f3aa70e4b23ea29847943d1ca8f
pham hong nham <70361369+hongnham@users.noreply.github.com>
2021-06-04 11:47:11 +0700

    Create status.go

---
+++ status.go
@@ -0,0 +1,11 @@
+type Status string
+func (s Status)StatusName(a, b, c, d string) int64 {
+  if s == "a" {
+		return 1
+	} else if s == "b" {
+		return 2
+	}else if s == "c" {
+		return 3
+	}
+	return 4
+}
